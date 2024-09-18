40ee743963643e003b51b3b09d2f6e6bfbfe3e21
KYs <amitkys59@gmail.com>
2024-09-19 04:32:11 +0530

    basic app setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,25 @@ function App(){
   const [count, setCount] = useState(0);
   return(
     <div>
-      <Count></Count>
+      <Count count={count}></Count>
       <Button></Button>
     </div>
   )
 }
 
-function Count(){
-
+function Count({count}){
+  return(
+    <div>{count}</div>
+  )
 }
 function Button(){
-
+  return(
+    <div>
+    <button>increase</button>
+    <button>decrease</button>
+    </div>
+    
+  )
 }
 
 export default App;
