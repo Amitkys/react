ac91cb59f3c806f9eb9035b5bc54244a4deaf28d
KYs <amitkys59@gmail.com>
2024-09-20 13:30:22 +0530

    count increament and decrement

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 
 
 function App() {
+  const [count, setCount] = useState(0);
   return(
     <div>
 
-      <h1>
-        Hello World this is amit 
-      </h1>
+     <Count></Count>
+     <Button></Button>
     </div>
   )
+}
+
+function Count(){
+
+}
+
+function Button(){
+
 }
 
 export default App;
