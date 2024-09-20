0592f761ff2811f674e72e4390fc7d43186705d4
KYs <amitkys59@gmail.com>
2024-09-20 16:15:24 +0530

    added countContext for providing data accross different component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import { CountContext } from "./context";
 
 // in this app use have to understand prop drilling problme
 // here you can saw:
 //count component does not use 'setCount' it's taking it and passing
 // into button component, that's why this is a problem, we have to fix it.
 function App() {
-  const [count, setCount] = useState(0);
+  <CountContext.Provider value={count}>
+    const [count, setCount] = useState(0);
+  </CountContext.Provider>
   return(
     <div>
 
@@ -14,22 +17,24 @@ function App() {
   )
 }
 
-function Count({count, setCount}){
+function Count({ setCount}){
   return(
     <di>
-      <CounteRenderer count = {count}></CounteRenderer>
-     <Button count={count} setCount = {setCount}></Button>   
+      <CounteRenderer></CounteRenderer>
+     <Button setCount = {setCount}></Button>   
     </di>
   )
 }
 
-function CounteRenderer({count}){
+function CounteRenderer(){
+  const count = 1;
   return(
     <h1>{count}</h1>
   )
 }
 
 function Button({count, setCount}){
+  const count = 1;
   return(
     <div>
       <button onClick={() =>{
