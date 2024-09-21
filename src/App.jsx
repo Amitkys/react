a2b6fdec5ebcf52a062d45ef36e86cc7a6f2ceae
KYs <amitkys59@gmail.com>
2024-09-21 07:49:22 +0530

    used recoil

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,44 @@
 import { useContext, useState } from "react";
 import { CountContext } from "./context";
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { countAtom } from "./store/atoms/count";
 
 // in this app use have to understand prop drilling problme
 // here you can saw:
 //count component does not use 'setCount' it's taking it and passing
 // into button component, that's why this is a problem, we have to fix it.
 function App() {
-  
-    const [count, setCount] = useState(0);
 
   return(
     <div>
-      <CountContext.Provider value={count}>
-      <Count  setCount = {setCount}></Count>
-     </CountContext.Provider>
+        <RecoilRoot>
+            <Count></Count>
+        </RecoilRoot>
     </div>
   )
 }
 
-function Count({ setCount}){
+function Count(){
     // problew with context API 
     // it re render (it not have any use);
     console.log('count rendered'); 
   return(
     <div>
       <CounteRenderer></CounteRenderer>
-     <Button setCount = {setCount}></Button>   
+     <Button></Button>   
     </div>
   )
 }
 
 function CounteRenderer(){
-  const count = useContext(CountContext);
+  const count = useRecoilValue(countAtom);
   return(
     <h1>{count}</h1>
   )
 }
 
-function Button({ setCount}){
-  const count = useContext(CountContext);
+function Button(){
+  const [count, setCount] = useRecoilState(countAtom);
   return(
     <div>
       <button onClick={() => {
