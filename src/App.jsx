020500bf3ffda4726c64c9be6043c264a1170611
KYs <amitkys59@gmail.com>
2024-09-21 08:57:56 +0530

    recoil useSetRecoil

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import { CountContext } from "./context";
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { countAtom } from "./store/atoms/count";
 
 // in this app use have to understand prop drilling problme
@@ -30,25 +30,41 @@ function Count(){
   )
 }
 
-function CounteRenderer(){
-  const count = useRecoilValue(countAtom);
-  return(
-    <h1>{count}</h1>
-  )
-}
+function CounteRenderer() {
+    const count = useRecoilValue(countAtom);
+  
+    return (
+      <div>
+        <h1>{count}</h1>
+        <Even></Even>
+      </div>
+    );
+  }
+  
 
 function Button(){
-  const [count, setCount] = useRecoilState(countAtom);
+//   const [count, setCount] = useRecoilState(countAtom); // not need this 
+    const setCount = useSetRecoilState(countAtom);
+    console.log('button re render')
   return(
     <div>
       <button onClick={() => {
-         setCount(count + 1);
+         setCount( count => count + 1);
       } }>increase</button>  
       <button onClick={() => {
-        setCount(count - 1)
+        setCount(count => count - 1)
       }}>decrement</button>
     </div>
   )
+}
+
+function Even(){
+    const count = useRecoilValue(countAtom);
+    return(
+        <div>
+            {(count % 2 == 0) ? "this is even": null}
+        </div>
+    )
 }
 
 export default App;
