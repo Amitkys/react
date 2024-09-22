ec7ad20048190a20469f676ad83bfb95cd5a0bc8
KYs <amitkys59@gmail.com>
2024-09-22 10:13:33 +0530

    optimal way to add data into exisint data using useRef

--- src/Components/AddTodo.jsx
+++ src/Components/AddTodo.jsx
@@ -1,11 +1,65 @@
+/*
+import { useState } from "react"
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { todoAtom } from "../store/atoms/todo";
+
+
 export function AddTodo(){
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    // Setter to update the Recoil atom state
+    const setTodos = useSetRecoilState(todoAtom);
+
+    function saveData() {
+        const newTodo = { title, description };
+        // Update Recoil state by adding new todo to the existing list
+        setTodos((oldTodos) => [...oldTodos, newTodo]);
+  }
+  console.log('hi form addtodo')
     return(
         <div>
             <lable>Add Title</lable> <br />
-            <input type="text" /> <br />
-            <lable>Add Description</lable> <br />
-            <input type="text"></input> <br />
-            <button>Save</button>
+            <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" /> <br />
+            <lable>Add Description</lable> <br/>
+            <input onChange={(e)=>setDescription(e.target.value)} value={description} type="text"></input> <br/>
+            <button onClick={saveData}>Save</button>
         </div>
     )
+} */
+
+// optimized way 
+
+
+
+import { useRef } from "react";
+import { useSetRecoilState } from "recoil";
+import { todoAtom } from "../store/atoms/todo";
+
+export function AddTodo() {
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+
+  const setTodos = useSetRecoilState(todoAtom);
+
+  function saveData() {
+    const newTodo = {
+      title: titleRef.current.value,
+      description: descriptionRef.current.value,
+    };
+    setTodos((oldTodos) => [...oldTodos, newTodo]);
+    // clear input field
+    titleRef.current.value = '';
+    descriptionRef.current.value = '';
+  }
+  console.log('checking re rendering in add todo component');
+  return (
+    <div>
+      <label>Add Title</label> <br />
+      <input ref={titleRef} type="text" /> <br />
+      <label>Add Description</label> <br />
+      <input ref={descriptionRef} type="text" /> <br />
+      <button onClick={saveData}>Save</button>
+    </div>
+  );
 }
