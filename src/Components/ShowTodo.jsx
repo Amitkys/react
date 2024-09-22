05f603a6048b9e9c58f612b71bd5b3f35365979d
KYs <amitkys59@gmail.com>
2024-09-22 09:18:17 +0530

    using another way to show data

--- src/Components/ShowTodo.jsx
+++ src/Components/ShowTodo.jsx
@@ -2,20 +2,20 @@ import { useRecoilValue } from "recoil"
 import { todoAtom } from "../store/atoms/todo"
 import React from "react";
 
-export function ShowTodo(){
-    let todos = useRecoilValue(todoAtom);
-    console.log(todos);
-    return(
-        <div>
-            {todos.map((todo, index) => {
-                return(
-                    <div key={index}>
-                    <h1>{todo.title} </h1>
-                    <h3>{todo.description}</h3>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+// export function ShowTodo(){
+//     let todos = useRecoilValue(todoAtom);
+//     console.log(todos);
+//     return(
+//         <div>
+//             {todos.map((todo, index) => {
+//                 return(
+//                     <div key={index}>
+//                     <h1>{todo.title} </h1>
+//                     <h3>{todo.description}</h3>
+//                     </div>
+//                 )
+//             })}
+//         </div>
+//     )
+// }
 
