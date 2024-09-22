d24f10722f8c2738f1afd10c04b43be6b2fadfe6
KYs <amitkys59@gmail.com>
2024-09-22 09:23:12 +0530

    both function is done with comments

--- src/Components/ShowTodo.jsx
+++ src/Components/ShowTodo.jsx
@@ -19,3 +19,23 @@ import React from "react";
 //     )
 // }
 
+export function ShowTodo(){
+    let todos = useRecoilValue(todoAtom);
+    return(
+        <>
+            {todos.map((todo, index) => (
+                <div key={index}>
+                    <h1>{todo.title}</h1>
+                    <h3>{todo.description}</h3>
+                </div>
+            ))}
+        </>
+    )
+}
+
+// difference between both of them:
+/* in first: we use {} after '=>' so we have to return value explicitly
+in second we do not use {} after '=>' instead i have use () so I do not need to return anythintg
+js code autometaclly will help to return things
+*/
+
