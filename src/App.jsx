c50bec787d3b219066de2f1d0e6f959dfc0fe2be
KYs <amitkys59@gmail.com>
2024-09-27 16:22:07 +0530

    routing here

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { BrowserRouter, Route, Router, Routes, useNavigate } from "react-router-dom";
+import Dashboard from './Components/Dashboard'
+import Landing from "./Components/Landing";
 
-function App(){
-  return(
+function App() {
+  return (
     <div>
-      <h1>This is main page</h1>
+      <h1>this is navbar</h1>
+
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} ></Route>
+          <Route path="/" element={<Landing />} ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
-};
+}
 
+function Appbar() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button onClick={() => navigate('/dashboard')}>dashbard</button>
+      <button onClick={() => navigate('/')}>Landing</button>
+
+    </div>
+  )
+}
 export default App;
