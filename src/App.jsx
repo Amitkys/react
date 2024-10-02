2858e70393cddcc0670e2e7e29227b59263883c0
KYs <amitkys59@gmail.com>
2024-10-02 23:53:35 +0530

    adde simple count app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-
+import {  useState } from "react";
 
 function App() {
+    let [count, setCount] = useState(0);
   return (
+    // const [count, setCount] = useState(0);
     <div>
-      <h1>This is maindfspage</h1>
+      <h1>this is main page</h1>
+      <button onClick={() => setCount(count + 4)}>{count}</button>
     </div>
   )
 }
